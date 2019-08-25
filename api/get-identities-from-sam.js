d4fc7822d637152d49f6bf9ac9c0099ee22837af
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-08-25 08:32:58 +0200

    Handles multiple sams (minor)

--- api/get-identities-from-sam.js
+++ api/get-identities-from-sam.js
@@ -6,18 +6,25 @@ const logger = require('../lib/logger')
 async function getIdentitiesFromSam (request, response, params) {
   const db = await mongo()
   const identities = db.collection(process.env.MONGODB_COLLECTION)
-  const { id: sam, old } = params
+  const { id: sam, old, sams } = params
   const key = `sam${old ? 'Old' : ''}`
-  logger('info', ['get-identities-from-sam', 'getIdentitiesFromSam', key, sam, 'start'])
-  try {
-    const document = await identities.findOne({ [key]: sam })
-    const status = document !== null ? 200 : 404
-    logger('info', ['get-identities-from-sam', 'getIdentitiesFromSam', key, sam, status])
-    response.json(fixDocument(document))
-  } catch (error) {
-    logger('error', ['get-identities-from-sam', 'getIdentitiesFromSam', key, sam, error])
-    response.status(500)
+  logger('info', ['get-identities-from-sam', 'getIdentitiesFromSam', key, `${sams ? sams.join(', ') : sam}`, 'start'])
+  if (!key && !sams) {
+    const error = new Error(`Missing required input`)
+    logger('error', ['api', 'get-identities-from-sam', 'getIdentitiesFromSam', `${sams ? sams.join(', ') : sam}`, error])
+    response.status(400)
     response.send(error)
+  } else {
+    try {
+      const document = sams ? await identities.findOne({ [key]: { $in: sams } }) : await identities.findOne({ [key]: sam })
+      const status = document !== null ? 200 : 404
+      logger('info', ['get-identities-from-sam', 'getIdentitiesFromSam', key, `${sams ? sams.join(', ') : sam}`, status])
+      response.json(fixDocument(document))
+    } catch (error) {
+      logger('error', ['get-identities-from-sam', 'getIdentitiesFromSam', key, `${sams ? sams.join(', ') : sam}`, error])
+      response.status(500)
+      response.send(error)
+    }
   }
 }
 
